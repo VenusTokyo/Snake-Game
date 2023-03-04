07d1399a1a7b8548973be2a1f1646b060130a650
Shrutika Shaw <66830887+VenusTokyo@users.noreply.github.com>
2023-03-04 21:38:44 +0530

    added food and snake head

---
+++ script.js
@@ -0,0 +1,17 @@
+const playBoard = document.querySelector(".play-board")
+
+let foodX, foodY;
+let snakeX=5, snakeY=10;
+const changeFoodPosition=()=>{
+    //passing a random 0- 30 value as food position
+    foodX=Math.floor(Math.random()*30)+1;
+    foodY=Math.floor(Math.random()*30)+1;
+}
+const initGame = () => {
+    let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+    htmlMarkup += `<div class="head" style="grid-area: ${snakeY} / ${snakeX}"></div>`;
+    //creating a food div and adding it to the play area
+    playBoard.innerHTML = htmlMarkup;
+}
+changeFoodPosition();
+initGame();
